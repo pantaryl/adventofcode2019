ec027b5855be5867fa19f8dfb89615d8efd4455b
Michael Grossfeld <Michael.Grossfeld@amd.com>
2019-12-05 11:37:32 -0500

    More intcode cleanup.

--- src/intcode.py
+++ src/intcode.py
@@ -36,35 +36,37 @@ class Intcode():
             instruction = "00000000000000" + str(self.program[self.ip])
             opcode      = int(instruction[-2:])
             if opcode in self.opcodes:
-                self.opcodes[opcode](instruction[:-2])
+                self.opcodes[opcode]([int(x) for x in instruction[:-2]])
 
         return self.retVal
 
-    def getOperand(self, value: int, mode: ParameterMode):
+    def getOperand(self, operand: int, modes: list):
+        value = self.program[self.ip + operand]
+        mode  = ParameterMode(modes[-operand])
         if mode == ParameterMode.Position:
             return self.program[value]
         elif mode == ParameterMode.Immediate:
             return value
 
-    def opcode1(self, modes: str):
-        src0 = self.getOperand(self.program[self.ip + 1], ParameterMode(int(modes[-1])))
-        src1 = self.getOperand(self.program[self.ip + 2], ParameterMode(int(modes[-2])))
+    def opcode1(self, modes: list):
+        src0 = self.getOperand(1, modes)
+        src1 = self.getOperand(2, modes)
         dest = self.program[self.ip + 3]
         if self.verbose:
             print(f"program[{dest}] = {src0}({self.program[self.ip + 1]}) + {src1}({self.program[self.ip + 2]})")
         self.program[dest] = src0 + src1
         self.ip += 4
 
-    def opcode2(self, modes: str):
-        src0 = self.getOperand(self.program[self.ip + 1], ParameterMode(int(modes[-1])))
-        src1 = self.getOperand(self.program[self.ip + 2], ParameterMode(int(modes[-2])))
+    def opcode2(self, modes: list):
+        src0 = self.getOperand(1, modes)
+        src1 = self.getOperand(2, modes)
         dest = self.program[self.ip + 3]
         if self.verbose:
             print(f"program[{dest}] = {src0}({self.program[self.ip + 1]}) * {src1}({self.program[self.ip + 2]})")
         self.program[dest] = src0 * src1
         self.ip += 4
 
-    def opcode3(self, modes: str):
+    def opcode3(self, modes: list):
         dest  = self.program[self.ip + 1]
         assert(len(self.inputStream) > 0)
         value = int(self.inputStream.pop(0))
@@ -73,16 +75,16 @@ class Intcode():
         self.program[dest] = value
         self.ip += 2
 
-    def opcode4(self, modes: str):
-        src0 = self.getOperand(self.program[self.ip + 1], ParameterMode(int(modes[-1])))
+    def opcode4(self, modes: list):
+        src0 = self.getOperand(1, modes)
         if self.verbose:
             print(f"Output: {src0}")
         self.retVal = src0
         self.ip += 2
 
-    def opcode5(self, modes: str):
-        src0    = self.getOperand(self.program[self.ip + 1], ParameterMode(int(modes[-1])))
-        jumpLoc = self.getOperand(self.program[self.ip + 2], ParameterMode(int(modes[-2])))
+    def opcode5(self, modes: list):
+        src0    = self.getOperand(1, modes)
+        jumpLoc = self.getOperand(2, modes)
         if self.verbose:
             print(f"jump-if-true - src0({src0}), destIp({jumpLoc})")
         if src0 != 0:
@@ -90,9 +92,9 @@ class Intcode():
         else:
             self.ip += 3
 
-    def opcode6(self, modes: str):
-        src0    = self.getOperand(self.program[self.ip + 1], ParameterMode(int(modes[-1])))
-        jumpLoc = self.getOperand(self.program[self.ip + 2], ParameterMode(int(modes[-2])))
+    def opcode6(self, modes: list):
+        src0    = self.getOperand(1, modes)
+        jumpLoc = self.getOperand(2, modes)
         if self.verbose:
             print(f"jump-if-false - src0({src0}), destIp({jumpLoc})")
         if src0 == 0:
@@ -100,18 +102,18 @@ class Intcode():
         else:
             self.ip += 3
 
-    def opcode7(self, modes: str):
-        src0    = self.getOperand(self.program[self.ip + 1], ParameterMode(int(modes[-1])))
-        src1    = self.getOperand(self.program[self.ip + 2], ParameterMode(int(modes[-2])))
+    def opcode7(self, modes: list):
+        src0    = self.getOperand(1, modes)
+        src1    = self.getOperand(2, modes)
         dest    = self.program[self.ip + 3]
         if self.verbose:
             print(f"less-than - program[{dest}] = 1 if {src0}({self.program[self.ip + 1]}) < {src1}({self.program[self.ip + 2]}) else 0")
         self.program[dest] = 1 if src0 < src1 else 0
         self.ip += 4
 
-    def opcode8(self, modes: str):
-        src0    = self.getOperand(self.program[self.ip + 1], ParameterMode(int(modes[-1])))
-        src1    = self.getOperand(self.program[self.ip + 2], ParameterMode(int(modes[-2])))
+    def opcode8(self, modes: list):
+        src0    = self.getOperand(1, modes)
+        src1    = self.getOperand(2, modes)
         dest    = self.program[self.ip + 3]
         if self.verbose:
             print(f"less-than - program[{dest}] = 1 if {src0}({self.program[self.ip + 1]}) == {src1}({self.program[self.ip + 2]}) else 0")
@@ -119,7 +121,7 @@ class Intcode():
         self.ip += 4
 
 
-    def opcode99(self, modes: str):
+    def opcode99(self, modes: list):
         self.ip  += 1
         self.eop  = True
 
