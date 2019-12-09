8ca45d5d99138109b5988bc44b8954dde48684bc
Michael Grossfeld <pantaryl@gmail.com>
2019-12-09 00:58:51 -0500

    Day9 Solution

--- src/intcode.py
+++ src/intcode.py
@@ -3,6 +3,7 @@ from enum import Enum
 class ParameterMode(Enum):
     Position  = 0
     Immediate = 1
+    Relative  = 2
 
 class Intcode():
     def __init__(self, verbose=False):
@@ -10,24 +11,31 @@ class Intcode():
         self.program     = []
         self.inputStream = []
         self.ip          = 0
+        self.relBase     = 0
         self.eop         = False
         self.needsInput  = False
         self.retVal = None
         self.opcodes = {
-            1 : self.opcode1,
-            2 : self.opcode2,
-            3 : self.opcode3,
-            4 : self.opcode4,
-            5 : self.opcode5,
-            6 : self.opcode6,
-            7 : self.opcode7,
-            8 : self.opcode8,
-            99: self.opcode99
+            1 : (self.opcode1, 3),
+            2 : (self.opcode2, 3),
+            3 : (self.opcode3, 1),
+            4 : (self.opcode4, 1),
+            5 : (self.opcode5, 2),
+            6 : (self.opcode6, 2),
+            7 : (self.opcode7, 3),
+            8 : (self.opcode8, 3),
+            9 : (self.opcode9, 1),
+            99: (self.opcode99, 0),
         }
+
+    def printOperandValues(self, numOperands:int):
+        if self.verbose and numOperands > 0:
+            print(",".join([str(x) for x in self.program[self.ip+1:self.ip+1+numOperands]]))
 
     def initProgram(self, program: list, inputStream: list = []):
         self.program     = list(program)
         self.ip          = 0
+        self.relBase     = 0
         self.eop         = False
         self.needsInput  = False
         self.retVal      = None
@@ -39,26 +47,38 @@ class Intcode():
             self.needsInput = False
 
         while self.eop is False and self.needsInput is False:
-            if self.verbose: print(f"IP: {self.ip} - ", end='')
             instruction = "00000000000000" + str(self.program[self.ip])
             opcode      = int(instruction[-2:])
-            if opcode in self.opcodes:
-                self.opcodes[opcode]([int(x) for x in instruction[:-2]])
+            assert(opcode in self.opcodes)
+            opcode = self.opcodes[opcode]
+            if self.verbose: print(f"IP: {self.ip} - {instruction} - Operands({self.printOperandValues(opcode[1])}) - ")
+            opcode[0]([int(x) for x in instruction[:-2]])
 
         return self.retVal
 
-    def getOperand(self, operand: int, modes: list):
+    def getOperand(self, operand: int, modes: list, isDest=False):
         value = self.program[self.ip + operand]
         mode  = ParameterMode(modes[-operand])
-        if mode == ParameterMode.Position:
-            return self.program[value]
-        elif mode == ParameterMode.Immediate:
-            return value
+        if isDest:
+            if mode == ParameterMode.Position:
+                return value
+            elif mode == ParameterMode.Relative:
+                return self.relBase + value
+        else:
+            if mode == ParameterMode.Position:
+                if self.verbose: print(f"(Operand {operand}, Mode {mode}) = {self.program[value]}")
+                return self.program[value]
+            elif mode == ParameterMode.Immediate:
+                if self.verbose: print(f"(Operand {operand}, Mode {mode}) = {value}")
+                return value
+            elif mode == ParameterMode.Relative:
+                if self.verbose: print(f"(Operand {operand}, Mode {mode}, RelBase {self.relBase}, Value {value}) = {self.program[self.relBase + value]}")
+                return self.program[self.relBase + value]
 
     def opcode1(self, modes: list):
         src0 = self.getOperand(1, modes)
         src1 = self.getOperand(2, modes)
-        dest = self.program[self.ip + 3]
+        dest = self.getOperand(3, modes, isDest=True)
         if self.verbose:
             print(f"program[{dest}] = {src0}({self.program[self.ip + 1]}) + {src1}({self.program[self.ip + 2]})")
         self.program[dest] = src0 + src1
@@ -67,7 +87,7 @@ class Intcode():
     def opcode2(self, modes: list):
         src0 = self.getOperand(1, modes)
         src1 = self.getOperand(2, modes)
-        dest = self.program[self.ip + 3]
+        dest = self.getOperand(3, modes, isDest=True)
         if self.verbose:
             print(f"program[{dest}] = {src0}({self.program[self.ip + 1]}) * {src1}({self.program[self.ip + 2]})")
         self.program[dest] = src0 * src1
@@ -77,7 +97,7 @@ class Intcode():
         if len(self.inputStream) == 0:
             self.needsInput = True
         else:
-            dest  = self.program[self.ip + 1]
+            dest  = self.getOperand(1, modes, isDest=True)
             assert(len(self.inputStream) > 0)
             value = int(self.inputStream.pop(0))
             if self.verbose:
@@ -115,7 +135,7 @@ class Intcode():
     def opcode7(self, modes: list):
         src0    = self.getOperand(1, modes)
         src1    = self.getOperand(2, modes)
-        dest    = self.program[self.ip + 3]
+        dest    = self.getOperand(3, modes, isDest=True)
         if self.verbose:
             print(f"less-than - program[{dest}] = 1 if {src0}({self.program[self.ip + 1]}) < {src1}({self.program[self.ip + 2]}) else 0")
         self.program[dest] = 1 if src0 < src1 else 0
@@ -124,11 +144,18 @@ class Intcode():
     def opcode8(self, modes: list):
         src0    = self.getOperand(1, modes)
         src1    = self.getOperand(2, modes)
-        dest    = self.program[self.ip + 3]
+        dest    = self.getOperand(3, modes, isDest=True)
         if self.verbose:
-            print(f"less-than - program[{dest}] = 1 if {src0}({self.program[self.ip + 1]}) == {src1}({self.program[self.ip + 2]}) else 0")
+            print(f"equals - program[{dest}] = 1 if {src0}({self.program[self.ip + 1]}) == {src1}({self.program[self.ip + 2]}) else 0")
         self.program[dest] = 1 if src0 == src1 else 0
         self.ip += 4
+
+    def opcode9(self, modes:list):
+        src0 = self.getOperand(1, modes)
+        if self.verbose:
+            print(f"set-rel-base - relBase = {self.relBase} + {src0}")
+        self.relBase += src0
+        self.ip += 2
 
 
     def opcode99(self, modes: list):
